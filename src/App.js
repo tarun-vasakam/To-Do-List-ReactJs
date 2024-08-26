405ed1029ed2fd32289c18a95e2d434dbe440b4f
Tarun Vasakam K <tarunkvasakam@gmail.com>
2024-08-27 01:01:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react';
+import './styles.css'
 
-function App() {
+const initialState = [];
+function todoReducer(state, action) {
+  switch (action.type) {
+    case 'ADD':
+      return [...state, action.payload];
+    case 'REMOVE':
+      return state.filter((_, index)=> index !== action.payload);
+    default:
+      throw new Error('Unknown action type');
+  }
+}
+function TodoApp() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAdd = () => {
+    if (inputValue.trim()) {
+      dispatch({ type: 'ADD', payload: inputValue });
+      setInputValue('');
+    }
+  };
+  const handleRemove = (index) => {
+    dispatch({ type: 'REMOVE', payload: index });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title-container">To-Do List</div>
+      <div className="input-container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)} size="40"/>
+        <button className="add-btn" onClick={handleAdd}>
+          Add
+        </button>
+      </div>
+      <ul className="list-container">
+        {state.map((task, index) => (
+          <li key={index} className="li-container">
+            {task}
+            <button className="remove-btn" onClick={() => handleRemove(index)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+export default TodoApp;
 
-export default App;
+ 
